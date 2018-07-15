db0d53fa5b8169ba2c82ccb0edfaa7d6b626b3c4
lingxufeng <lingxufeng@bytedance.com>
2018-07-15 16:19:35 +0800

    test

--- demo/app.js
+++ demo/app.js
@@ -1,28 +1,28 @@
-const { render, hyperscript, setState } = require('../src')
+const {render, hyperscript, setState} = require('../src');
 
 function createComponent(count) {
-  const r = [...Array(count).keys()]
-  return (
-    <ul id="cool" className={`my-class-${count}`}>
-      { r.map(n => <li>{count}</li>) }
-    </ul>
-  )
+    const r = [...Array(count).keys()];
+    return (
+        <ul id="cool" className={`my-class-${count}`}>
+            {r.map(n => <li>{count}</li>)}
+        </ul>
+    );
 }
 
 function inc(parent, count) {
-  if (count > 20) return
-  setState(parent, createComponent(count), createComponent(count + 1))
+    if (count > 10) return;
+    setState(parent, createComponent(count), createComponent(count + 1));
 
-  setTimeout(() => inc(parent, count + 1), 500)
+    setTimeout(() => inc(parent, count + 1), 500);
 }
 
 // test setState
-(function() {
-  const root = document.getElementById('root')
-  render(createComponent(1), root)
+(function () {
+    const root = document.getElementById('root');
+    render(createComponent(1), root);
 
-  setTimeout(() => inc(root, 1), 500)
-})()
+    setTimeout(() => inc(root, 1), 500);
+})();
 
 // test mount
 // (function() {
--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-const mountElement = require('./mountElement')
-const diff = require('./diff')
-const patch = require('./patch')
-const { setProps } = require('./DOM')
+const mountElement = require('./mountElement');
+const diff = require('./diff');
+const patch = require('./patch');
+const {setProps} = require('./DOM');
 
 function render(element, node) {
-  node.appendChild(mountElement(element))
+    console.log(element);
+    node.appendChild(mountElement(element));
 }
 
 function hyperscript(type, props, children) {
-  props = props || {}
+    props = props || {};
 
-  // children could be both primitive type like string or number,
-  // or an array of element
-  return {
-    type,
-    props,
-    children
-  }
+    // children could be both primitive type like string or number,
+    // or an array of element
+    return {
+        type,
+        props,
+        children
+    };
 }
 
-/** 
+/**
  * This is actually not `setState` at all,
  * it's just the entry for the diff algorithm.
  * In react, setState is the entry so here we keep the consistency
-*/
+ */
 function setState(parent, prevElement, nextElement) {
-  const p = diff(prevElement, nextElement)
-  console.log(p)
-  patch(parent, p)
+    const p = diff(prevElement, nextElement);
+    console.log(p);
+    patch(parent, p);
 }
 
 module.exports = {
-  hyperscript,
-  render,
-  setState
-}
+    hyperscript,
+    render,
+    setState
+};
